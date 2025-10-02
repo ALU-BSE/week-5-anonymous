16c8fbd275198a374dda2f707f0201d49796d55e
Lisky-pixel <o.ndukaaku@alustudent.com>
2025-10-02 11:18:15 +0200

    configured the urls

--- users/serializers.py
+++ users/serializers.py
@@ -5,19 +5,22 @@ from django.contrib.auth.password_validation import validate_password
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for user profile"""
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'phone_number', 'user_type']
+        fields = ('id', 'email', 'first_name', 'last_name',
+                  'phone_number', 'user_type', 'date_joined')
+        read_only_fields = ('id', 'date_joined')
 
 
 class PassengerSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
-    
+
     class Meta:
         model = Passenger
         fields = [
-            'id', 'user', 'passenger_id', 'preferred_payment_method', 
-            'home_address', 'profile_picture', 'preferred_language', 
+            'id', 'user', 'passenger_id', 'preferred_payment_method',
+            'home_address', 'profile_picture', 'preferred_language',
             'emergency_contact', 'is_verified', 'created_at', 'updated_at'
         ]
         read_only_fields = ['id', 'created_at', 'updated_at']
@@ -25,31 +28,33 @@ class PassengerSerializer(serializers.ModelSerializer):
 
 class RiderSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
-    
+
     class Meta:
         model = Rider
         fields = [
             'id', 'user', 'profile_picture', 'license_number', 'license_picture',
             'id_number_picture', 'verification_status', 'verification_notes',
-            'is_available', 'current_latitude', 'current_longitude', 
-            'average_rating', 'total_rides', 'total_earnings', 'created_at', 'updated_at'
+            'is_available', 'current_latitude', 'current_longitude',
+            'average_rating', 'created_at', 'updated_at'
         ]
-        read_only_fields = ['id', 'average_rating', 'total_rides', 'total_earnings', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'average_rating', 'created_at', 'updated_at']
+
 
 User = get_user_model()
+
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     """Serializer for user registration"""
     password = serializers.CharField(
-        write_only=True, 
-        required=True, 
+        write_only=True,
+        required=True,
         validators=[validate_password]
     )
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'password2', 'first_name', 
+        fields = ('email', 'password', 'password2', 'first_name',
                   'last_name', 'phone_number', 'user_type')
         extra_kwargs = {
             'first_name': {'required': True},
@@ -67,12 +72,3 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
         return user
-
-
-class UserSerializer(serializers.ModelSerializer):
-    """Serializer for user profile"""
-    class Meta:
-        model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 
-                  'phone_number', 'user_type', 'date_joined')
-        read_only_fields = ('id', 'date_joined')
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from users.views import UserViewSet, PassengerViewSet, RiderViewSet, RegisterView
+from users.views import (
+    UserViewSet,
+    PassengerViewSet,
+    RiderViewSet,
+    RegisterView,
+    LoginView,
+    UserProfileView,
+)
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -10,4 +18,7 @@ router.register(r'riders', RiderViewSet, basename='rider')
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 ] + router.urls
